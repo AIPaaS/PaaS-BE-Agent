7d114273c099b4690bc11b4d306447073d863b38
bixy <bixy@bogon>
2016-03-17 16:30:44 +0800

    share new agent

---
+++ src/main/java/com/ai/platform/agent/web/main/JettyMain.java
@@ -0,0 +1,16 @@
+package com.ai.platform.agent.web.main;
+
+
+
+public class JettyMain {
+	
+	
+	
+
+	public static void main(String[] args) {
+		JettyServer server = new JettyServer();
+		server.start();
+	}
+	
+	
+}
