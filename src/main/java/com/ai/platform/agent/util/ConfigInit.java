f3a25684f2d864945658b01b99c897a4027d7d36
zhangzhongde <xtgjzzd@163.com>
2016-03-21 21:11:09 +0800

    配置文件初始化加载服务端跟客户端加载文件属性

--- src/main/java/com/ai/platform/agent/util/ConfigInit.java
+++ src/main/java/com/ai/platform/agent/util/ConfigInit.java
@@ -24,9 +24,17 @@ public class ConfigInit {
 					ConfigInit.class.getClassLoader().getResourceAsStream(AgentConstant.AGENT_CONFIG_FILE_NAME),
 					"UTF-8"));
 			p.load(bf);
+			String keyStr = null;
 			for (Entry<Object, Object> tmpEntry : p.entrySet()) {
-				serverConstant.put((String) tmpEntry.getKey(), (String) tmpEntry.getValue());
-				logger.info("init key[{}] value {{}}", (String) tmpEntry.getKey(), (String) tmpEntry.getValue());
+				if(((String)tmpEntry.getKey()).startsWith("netty")){
+					keyStr = ((String)tmpEntry.getKey()).substring("netty.".length());
+					serverConstant.put(keyStr, (String) tmpEntry.getValue());
+					logger.info("init key[{}] value {{}}", (String) keyStr, (String) tmpEntry.getValue());
+				}else if(((String)tmpEntry.getKey()).startsWith("client")){
+					keyStr = ((String)tmpEntry.getKey()).substring("client.".length());
+					serverConstant.put(keyStr, (String) tmpEntry.getValue());
+					logger.info("init key[{}] value {{}}", (String) keyStr, (String) tmpEntry.getValue());
+				}
 			}
 		} catch (IOException e) {
 			logger.error("{} config properties read exception !!!");
